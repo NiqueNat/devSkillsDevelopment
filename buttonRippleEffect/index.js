f73e3d7f0657027a39b0d5d180a0bc017d56388d
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-19 12:05:47 -0400

    Button Ripple Effect

---
+++ buttonRippleEffect/index.js
@@ -0,0 +1,8 @@
+const btnEI = document.querySelector(".btn");
+
+btnEI.addEventListener("mouseover", (event) => {
+  const x = event.pageX - btnEI.offsetLeft;
+  const y = event.pageY - btnEI.offsetTop;
+  btnEI.style.setProperty("--xPos", x + "px");
+  btnEI.style.setProperty("--yPos", y + "px");
+});
