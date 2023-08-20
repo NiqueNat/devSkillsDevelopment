f8f3b367e7c75f3fb10a502bfdc9e4ec7195b4b2
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-20 12:25:42 -0400

    Text-animation

---
+++ photo-text-affect/index.js
@@ -0,0 +1,24 @@
+const  container = document.querySelector('.container');
+
+const careers =["Designer", "Programmer", "Web Developer", "Freelance", "Full-Stack Developer", "Front-End Developer", "Back-End Developer", "UI/UX Designer", "Graphic Designer", "Digital Effects Artist"]
+
+let careersIndex = 0;
+let characterIndex = 0;
+
+function updateText() {  
+    characterIndex++;
+    container.innerHTML = `<h1>I am a ${careers[careersIndex].slice(0, characterIndex)}</h1>`;
+
+
+
+if(characterIndex === careers[careersIndex].length) {
+    careersIndex++;
+   characterIndex = 0;
+}
+if(careersIndex === careers.length) {
+    careersIndex = 0;
+}
+setTimeout(updateText, 400);
+};
+
+updateText();
