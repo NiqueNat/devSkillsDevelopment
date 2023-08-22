76b8f4a84944657e858a4344e7bc2a4965b20fad
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-22 07:37:41 -0400

    Comments added

--- weatherapp/index.js
+++ weatherapp/index.js
@@ -5,29 +5,14 @@ const weatherData = document.getElementById("weather-data");
 const cityInput = document.getElementById("city-input");
 const form = document.querySelector("form");
 
+//Set default city
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const city = cityInput.value;
   getWeatherData(city);
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
-  // fetch(url)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //     console.log(data);
-  //     const { name } = data;
-  //     const { temp } = data.main;
-  //     const { description, icon } = data.weather[0];
-  //     weatherData.innerHTML = `
-  //         <div class="weather-data">
-  //             <h2 class="city-name" data-name="${name}">${name}</h2>
-  //             <div class="temp">${Math.round(temp)}°C</div>
-  //             <div class="description">${description}</div>
-  //             <div class="icon"><img src="https://openweathermap.org/img/wn/${icon}.png"></div>
-  //         </div>
-  // `;
-  // });
 });
 
+//Get weather data
 async function getWeatherData(city) {
   try {
     const response = await fetch(
@@ -47,13 +32,15 @@ async function getWeatherData(city) {
         `Wind speed: ${data.wind.speed} km/h`,
     ]
 
-    // weatherData.querySelector(".city-name").innerText = city;
+//Display weather data
     weatherData.querySelector(".temperature").textContent = `${temperature}°C`;
     weatherData.querySelector(".description").textContent = description;
     weatherData.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png">`;
      weatherData.querySelector(".details").innerHTML = details.map((detail) => `<div>${detail}</div>`).join("");
 
-  } catch (error) {
+  } 
+  //Error handling
+  catch (error) {
     weatherData.querySelector(".temperature").textContent = "";
 
     weatherData.querySelector(".description").textContent = "An error has occured, please try again later";
