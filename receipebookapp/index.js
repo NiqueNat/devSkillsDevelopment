9dfab063d3abf1b5f25a2b496b234321cfe71472
NiqueVanier <70446500+NiqueNat@users.noreply.github.com>
2023-08-25 06:42:56 -0400

    Update index.js

--- receipebookapp/index.js
+++ receipebookapp/index.js
@@ -1,27 +1,28 @@
 const API_KEY = '3f5c0394b45647de8dc1906ba1c71926';
 const recipeList = document.getElementById('recipe-list');
 
+//displayRecipes function
 function displayRecipes(recipes) {
   recipeList.innerHTML = '';
   recipes.forEach((recipe) => {
     const recipeItem = document.createElement('li');
-    recipeItem.classList.add('recipe-item'); // Changed 'classlist' to 'classList'
+    recipeItem.classList.add('recipe-item'); 
     
-    const recipeImage = document.createElement('img'); // Added 'const' before recipeImage
+    const recipeImage = document.createElement('img');
     recipeImage.src = recipe.image;
     recipeImage.alt = 'recipe';
     
-    const recipeTitle = document.createElement('h2'); // Added 'const' before recipeTitle
+    const recipeTitle = document.createElement('h2'); 
     recipeTitle.innerText = recipe.title;
     
-    const recipeIngredients = document.createElement('p'); // Added 'const' before recipeIngredients
+    const recipeIngredients = document.createElement('p');
     recipeIngredients.innerHTML = `<strong>Ingredients:</strong> ${recipe.extendedIngredients.map((ingredient) => ingredient.original).join(', ')}`; // Changed 'orginal' to 'original'
     
-    const recipeLink = document.createElement('a'); // Added 'const' before recipeLink
+    const recipeLink = document.createElement('a'); 
     recipeLink.href = recipe.sourceUrl;
     recipeLink.innerText = 'View Recipe';
     
-    recipeItem.appendChild(recipeTitle); // Reordered elements for a better layout
+    recipeItem.appendChild(recipeTitle); 
     recipeItem.appendChild(recipeImage);
     recipeItem.appendChild(recipeIngredients);
     recipeItem.appendChild(recipeLink);
@@ -29,12 +30,14 @@ function displayRecipes(recipes) {
   });
 }
 
+//getRecipes function
 async function getRecipes() {
   const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`);
   const data = await response.json();
   return data.recipes;
 }
 
+//init function
 async function init() {
   try {
     const recipes = await getRecipes();
